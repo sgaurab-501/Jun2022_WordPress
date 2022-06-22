1625f8d2ef0b53ed6e87d3b4228c91c30d46f3d1
Gaurab Sharma <sgaurab001@gmail.com>
2022-06-23 00:36:35 +0530

    First commit on June 23, 2022

---
+++ src/main/java/com/qa/wordpress/pages/BlockedSitesPage.java
@@ -0,0 +1,13 @@
+package com.qa.wordpress.pages;
+
+public class BlockedSitesPage {
+
+	public static void main(String[] args) {
+		System.out.println("feature f1");
+	System.out.println("feature f2");
+	
+	
+	}
+	
+	
+}
